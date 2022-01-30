5b0e485b045eb170da5b60e8571c606ca03981d1
czM1K3 <misamadera1@gmail.com>
2022-01-30 16:51:43 +0100

    Projects

---
+++ src/components/project.tsx
@@ -0,0 +1,29 @@
+import { useTranslations } from "next-intl";
+import React, { FC } from "react";
+import { Project } from "../types/project";
+import styles from "../styles/projects.module.scss";
+import Image from "next/image";
+
+type ProjectProps = Project;
+
+const Project: FC<ProjectProps> = ({ description, image, title, url, download, git }) => {
+	const t = useTranslations("Projects");
+	return (
+		<div className="card">
+			<div className="card-image">
+				<Image src={image} layout="responsive" sizes="30vw" width="10%" height="10%" alt={t(title)} className={styles.image} />
+				<span className={"card-title " + styles.shadow}>{t(title)}</span>
+			</div>
+			<div className="card-content">
+				<p>{t(description)}</p>
+			</div>
+			<div className="card-action">
+				{download && <a href={download}>{t("download")}</a>}
+				{url && <a href={url}>{t("link")}</a>}
+				{git && <a href={git}>{t("git")}</a>}
+			</div>
+		</div>
+	);
+};
+
+export default Project;
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,20 +1,45 @@
 import type { GetStaticProps, NextPage } from "next";
 import Layout from "../components/layout";
 import { useTranslations } from "next-intl";
+import { Project as ProjectType } from "../types/project";
+import ProjectList from "../data/projects";
+import Project from "../components/project";
+import styles from "../styles/projects.module.scss";
+import { orderByName } from "../utils/order";
 
-const Projects: NextPage = () => {
+type ProjectsProps = {
+  messages: any;
+  projects: ProjectType[];
+};
+
+const Projects: NextPage<ProjectsProps> = ({ projects }) => {
   const t = useTranslations("Projects");
   return (
     <Layout>
-      {t("title")}
+      <h1 className={styles.heading}>{t("title")}</h1>
+      <div className={styles.flex}>
+        {projects.sort((a, b) => orderByName(t(a.title), t(b.title))).map((project) => (
+          <div className={styles.item} key={project.title}>
+            <Project
+              description={project.description}
+              image={project.image}
+              title={project.title}
+              url={project.url}
+              download={project.download}
+              git={project.git}
+            />
+          </div>
+        ))}
+      </div>
     </Layout>
   );
 };
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
+export const getStaticProps: GetStaticProps<ProjectsProps> = async ({ locale }) => {
   return {
     props: {
-      messages: (await import (`../../translations/${locale}.json`)).default,
+      messages: (await import(`../../translations/${locale}.json`)).default,
+      projects: ProjectList,
     },
   };
 };
