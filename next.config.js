01a52878474676aa5878b67cb7d117a5a422b86e
czM1K3 <misamadera1@gmail.com>
2022-01-31 17:47:25 +0100

    Preact

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
     locales: ["cs", "en"],
     defaultLocale: "cs",
   },
+  webpack: (config, { dev, isServer}) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
