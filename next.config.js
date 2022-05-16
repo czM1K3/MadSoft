d8364dbd0bf8090f9f3e1642b791a35fcc7a8611
czM1K3 <misamadera1@gmail.com>
2022-05-16 18:51:26 +0200

    Removed Preact

--- next.config.js
+++ next.config.js
@@ -5,16 +5,6 @@ const nextConfig = {
     locales: ["cs", "en"],
     defaultLocale: "cs",
   },
-  webpack: (config, { dev, isServer}) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
